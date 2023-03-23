f4821b792afdd78b4b85a15de884a6eb22e82518
Kirk Newson <kirk.newson@gmail.com>
2023-03-24 11:59:01 +1300

    Fixed up Script

--- docs/build-coder.sh
+++ docs/build-coder.sh
@@ -1,9 +1,9 @@
 #Requires - Ubuntu 22.04 +
 ## Install WSL2
-#Maybe 
-dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
+#Maybe - wsl command line works by default
+#dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
 
-# Install non-defaul Ubuntu 22.04
+# Install non-default Ubuntu 22.04
 wsl --install -d Ubuntu_22.04
 #if it exists
 wsl --set-default -d Ubuntu_22.04
@@ -16,8 +16,8 @@ wsl --set-default -d Ubuntu_22.04
 # 2. Preferences - Add integrations to WSL.
 
 # Clone Coder
-git clone --branch dean/tunnelsdk https://github.com/coder/coder.git ~/coder
-
+#git clone --branch dean/tunnelsdk https://github.com/coder/coder.git ~/coder
+git clone https://github.com/coder/coder.git ~/coder
 
 #export CGO_ENABLED=1
 
@@ -39,8 +39,8 @@ protoc --version  # Ensure compiler version is 3+
 
 curl -sLO https://go.dev/dl/go1.20.2.linux-amd64.tar.gz
 sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.20.2.linux-amd64.tar.gz
-echo 'export PATH=$PATH:/usr/local/go/bin:~/go.bin' >> ~/.bashrc
-
+echo 'export PATH=$PATH:/usr/local/go/bin:~/go/bin' >> ~/.bashrc
+## NEW SHELL
 go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
 go install storj.io/drpc/cmd/protoc-gen-go-drpc@latest
 
@@ -48,4 +48,4 @@ go install github.com/kyleconroy/sqlc/cmd/sqlc@latest
 
 
 cd ~/coder
-make
+make build/coder_linux_amd64
